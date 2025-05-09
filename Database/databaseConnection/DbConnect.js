bf14ddd6c80532b0069b8766757c363b5f3245fd
waqas837 <waqaskhanbughlani1124@gmail.com>
2025-05-09 20:31:14 +0500

    new build test-githubactions-1

--- Database/databaseConnection/DbConnect.js
+++ Database/databaseConnection/DbConnect.js
@@ -4,8 +4,7 @@ const mysql = require("mysql2/promise");
 const pool = mysql.createPool({
   host: "localhost",
   user: "root",
-  // password: "root",
-  password: "",
+  password: "root",
   database: "cryptocurr",
   waitForConnections: true,
   connectionLimit: 10, // Adjust as needed
