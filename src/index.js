b2e6080f5bb5cba75159f552990b70868999402f
waqas837 <waqaskhanbughlani1124@gmail.com>
2025-05-10 21:59:21 +0500

    api urls changed

--- src/index.js
+++ src/index.js
@@ -70,6 +70,7 @@ app.post(
               "SELECT * FROM users WHERE id=?",
               [userId]
             );
+            console.log("Notice: resulst.rows[0] 00", resulst.rows[0])
             const key = resulst.rows[0].BalanceApiKey;
             console.log("resulst", resulst.rows[0].BalanceApiKey);
             await topUpCaptchaBalance(key, amount);
@@ -136,6 +137,7 @@ app.post("/webhook", async (req, res) => {
       let resulst = await connection.query("SELECT * FROM users WHERE id=?", [
         userid,
       ]);
+      console.log("Notice: resulst.rows[0]:1", resulst.rows[0])
       const key = resulst.rows[0].BalanceApiKey;
       console.log("resulst", resulst.rows[0].BalanceApiKey);
       await topUpCaptchaBalance(key, paidAmount);
