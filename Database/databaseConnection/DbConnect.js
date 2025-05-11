8a9a1f3bc8e198350ea12c42dcdd562734b1e333
waqas837 <waqaskhanbughlani1124@gmail.com>
2025-05-11 23:33:15 +0500

    db

--- Database/databaseConnection/DbConnect.js
+++ Database/databaseConnection/DbConnect.js
@@ -4,8 +4,8 @@ const mysql = require("mysql2/promise");
 const pool = mysql.createPool({
   host: "localhost",
   user: "root",
-  // password: "root",
-  password: "",
+  password: "root",
+  // password: "",
   database: "cryptocurr",
   waitForConnections: true,
   connectionLimit: 10, // Adjust as needed
